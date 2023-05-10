933b0014c5f1c57f1d412b83f97ee03c5e62b3bd
epodak <epodak@gmail.com>
2023-05-10 11:18:02 +0800

    在这个类及调用中学习到了 1，类的构造，他包括constructor和this 2，类中的方法可以用this.method()调用。 3，文本可以通过${}占位符来标识。 4，其他的return基本上和Python差不多。 5，不过this与self不完全一样，还是有不少差别

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-console.log('Loading function');
+class Person {firstName;lastName;age;
+    constructor(firstName,lastName,age){
+        this.firstName=firstName;
+        
+        this.lastName=lastName; 
+        
+        this.age=age;  
+
+    }
+    getNames(){
+        const name =  `${this.firstName} ${this.lastName}`
+        return name;
+    }
+    getAge(){
+        return this.age;
+    }
+    sayHello(){
+        
+        return `Hello ${this.getNames()} and your age is ${this.getAge()}`
+    }
+}
+
+
+const person = new Person('John','Smith',30);
+const text = person.sayHello();
+console.log(text);
