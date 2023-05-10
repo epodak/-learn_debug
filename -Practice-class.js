22af581dc5e6f3885de9ab2516f5b0d4c57a4567
epodak <epodak@gmail.com>
2023-05-10 13:44:29 +0800

    ChatGPT说这种写法不好，给我讲了一堆冒泡的东西 因为即使年龄为负，Person 对象仍然会被创建，这可能会导致一些不可预见的结果。
    一种更好的方法是让错误冒泡上来，让创建 Person 对象的代码处理错误

--- -Practice-class.js
+++ -Practice-class.js
@@ -25,29 +25,38 @@
     在 `Person` 类的构造函数中添加异常处理。如果试图创建一个年龄为负数的 `Person`，则抛出一个错误，并在控制台中打印出错误信息。 */
 
 class Person {
-    name;
-    age;
-    hobby;
-    constructor(name, age,hobby) {
-        this.name = name;
-        this.age = age;
-        this.hobby = hobby;
+    constructor(name, age, hobby) {
+        try {
+            this.name = name;
+            this.hobby = hobby;
+            this.age = age;
+            if (age < 0) {
+                throw new Error('Age cannot be negative');
+            }
+            
+        } catch (error) {
+            console.log(error.message);
+            // 这里可以添加处理错误的代码，例如设置年龄为默认值
+            this.age = 0;
+            
+        }
     }
     greet() {
-        return `Hello, my name is ${this.name} and I am ${this.age} years old.and I love ${this.hobby}`;
+        return `Hello, my name is ${this.name} and I am ${this.age} years old. I love ${this.hobby}`;
     }
     birthday() {
         this.age++;
         return this.greet();
     }
 }
+
 // const john = new Person('John', 30,'reading');
 // console.log(`${greet()}`);
 // const text = john.birthday();
 // console.log(text);
 
 let people = [
-    new Person('John', 30,'reading'),
+    new Person('John', -30,'reading'),
     new Person('Tom', 25,'swimming'),
     new Person('Jane', 20,'singing'),
 ]
